a05d40b99575db68dda9b8d77d36eb7b8f7da131
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-10-27 10:08:45 +0200

    print of array length

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 string[] favouriteFoods = { "Pizza", "Risotto ai Funghi", "Pasta alla Carbonara", "Tagliata di Manzo", "Baccalà Mantecatto" };
+Console.WriteLine(favouriteFoods.Length);
