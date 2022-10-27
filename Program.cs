61400eb1e9550e2191457f42864ddff52ec374c7
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-10-27 10:10:50 +0200

    print of all elements of the array

--- Program.cs
+++ Program.cs
@@ -2,3 +2,9 @@
 Console.WriteLine("Hello, World!");
 string[] favouriteFoods = { "Pizza", "Risotto ai Funghi", "Pasta alla Carbonara", "Tagliata di Manzo", "Baccalà Mantecatto" };
 Console.WriteLine(favouriteFoods.Length);
+int index = 0;
+
+for(; index < favouriteFoods.Length; index++)
+{
+    Console.WriteLine(favouriteFoods[index]);
+}
